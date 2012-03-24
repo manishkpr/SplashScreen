8d01b4361eceae896eccb445595b2b96c66f8a6b
munish <manishkpr@gmail.com>
2012-03-24 16:02:00 +0530

    FancySplashScreen v1.0

---
+++ SpalshScreen/src/com/fancy/splashscreen/SpalshScreenActivity.java
@@ -0,0 +1,42 @@
+package com.fancy.splashscreen;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class SpalshScreenActivity extends Activity {
+	 public void onAttachedToWindow() {
+			super.onAttachedToWindow();
+			Window window = getWindow();
+			window.setFormat(PixelFormat.RGBA_8888);
+		}
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+        
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        
+        
+        
+    }
+   
+}
